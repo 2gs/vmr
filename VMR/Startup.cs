a242a26a3b1e7138001267f7454599f24533866b
Ernest Dicks <ernestdicks015@gmail.com>
2017-10-31 13:38:30 -0400

    Initial commit

---
+++ VMR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VMR.Startup))]
+namespace VMR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
