d55eabb4f26f6b59e50a28d9720e6ef138c442d6
RenaTin1 <31945448+RenaTin1@users.noreply.github.com>
2017-11-02 13:36:09 -0400

    Updated Payment form

--- VMR/Scripts/Site.js
+++ VMR/Scripts/Site.js
@@ -2,3 +2,5 @@
     $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
     $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
 });
+
+
